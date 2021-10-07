57c0fc87f8441a86734bc039b65df28c8c415342
JosephineOG <90419012+JosephineOG@users.noreply.github.com>
2021-10-07 21:16:03 +0100

    Update main.js
    Add practice comments, variables, console logs, docyment.write and datatypes

---
+++ module-3/task/start/scripts/main.js
@@ -0,0 +1,42 @@
+// This is a single line comment
+
+/**
+ * This is a multiple
+ * line comment
+ **/
+
+/**
+ * let age =37;  
+ * const name= 'Luke';
+ * var address= '123 long avenue'
+ * let means value can change
+ * const means it stays constant
+ * var is outdated but we'll still see it in old code.
+  *
+  *let $variable = 0;
+  *let variable =);
+  * let_variable =0;
+  *
+  *let firstName ="Luke"
+  *let lastName ="Ashton-Johnson "
+*/
+
+const productName = 'Baked Beans';
+let price= 1.27;
+let quantity= 4;
+let InStock = true;
+let discountAmount= 10;
+
+//console.log ( 'Product Name',productName );
+//console.log( 'Price',price );
+//console.log ( 'Quantity', quantity ) 
+//console.log( 'InStock',InStock );
+//console.log ( 'discount amount',discountAmount );
+
+//console.log( typeof productName );
+//console.log( typeof price );
+//console.log ( typeof quantity );
+//console.log( typeof inStock );
+//console.log( typeof discountAmount );
+
+//document.write( "<p>" + productName + "</p>" );
